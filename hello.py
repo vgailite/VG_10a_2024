9b4afb5166a54aa2c4e045ad79bacc8b9b45734c
vgailite <viktorija.gailite11@gmail.com>
2024-10-10 11:28:46 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+print('Sveiks, "labo skolēn"!')
+print("hello, \"friend\"")
+print("Viktorija", "Gailite", sep='?????')
+name = input("Kā tevi sauc? ")
+
+name = name.strip().title()
+
+
+print("Sveiks,", name)
+print(f"Sveiks, {name}")
