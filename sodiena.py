d3c75ba5130f375972f43d68d247ebdc73101325
vgailite <viktorija.gailite11@gmail.com>
2024-11-21 11:29:52 +0200

    Update sodiena.py

--- sodiena.py
+++ sodiena.py
@@ -2,26 +2,26 @@ import os
 os.system('cls')
 print("\n")
 
-# #x= int(input("Kāds ir x?"))
-# #y = int(input("Kāds ir y?"))
+x= int(input("Kāds ir x?"))
+y = int(input("Kāds ir y?"))
 
-# #if x != y:
-#     #print("x nav vienāds ar y")
-# #else:
-#     #print("x ir vienāds ar y")
+if x != y:
+    print("x nav vienāds ar y")
+else:
+    print("x ir vienāds ar y")
 
-# punkti= int(input("Punkti:"))
+punkti= int(input("Punkti:"))
 
-# if punkti >= 90 and punkti <= 100:
-#     print("Vērtejums: 10")
-# elif punkti >= 80 and punkti < 90:
-#     print("Vērtejums: 9")
-# elif punkti >= 70 and punkti < 80:
-#     print("Vērtejums: 8")
-# elif punkti >= 60 and punkti < 70:
-#     print("Vērtejums: 7")
-# else:
-#     print("Vērtējums: n/v")
+if punkti >= 90 and punkti <= 100:
+    print("Vērtejums: 10")
+elif punkti >= 80 and punkti < 90:
+    print("Vērtejums: 9")
+elif punkti >= 70 and punkti < 80:
+    print("Vērtejums: 8")
+elif punkti >= 60 and punkti < 70:
+    print("Vērtejums: 7")
+else:
+    print("Vērtējums: n/v")
 
 
 import math
